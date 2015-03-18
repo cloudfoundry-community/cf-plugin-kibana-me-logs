cdd90401e3e19565ef82c095fc4b036b25e200ec
Dr Nic Williams <drnicwilliams@gmail.com>
2015-03-18 15:32:31 -0700

    raise error if target app isn't bound to logstash14 service

--- plugin.go
+++ plugin.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 
@@ -14,7 +14,15 @@ import (
 
 func fatalIf(err error) {
 	if err != nil {
-		log.Fatalln("error:", err)
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+}
+
+func fatalWithMessageIf(err error, msg string) {
+	if err != nil {
+		fmt.Println("ERROR:", msg)
+		os.Exit(1)
 	}
 }
 
@@ -31,6 +39,20 @@ type appSearchResults struct {
 	Resources []appSearchResources `json:"resources"`
 }
 
+type appEnv struct {
+	System map[string]interface{} `json:"system_env_json"`
+}
+
+type appEnvService struct {
+	Name        string                 // name of the service
+	Label       string                 // label of the service
+	Tags        []string               // tags for the service
+	Plan        string                 // plan of the service
+	Credentials map[string]interface{} // credentials for the service
+}
+
+type appEnvServices map[string][]appEnvService
+
 // KibanaMeAppPlugin is the type for the plugin functions
 type KibanaMeAppPlugin struct{}
 
@@ -46,13 +68,15 @@ func (c *KibanaMeAppPlugin) Run(cliConnection plugin.CliConnection, args []strin
 
 	if args[0] == "kibana-me-logs" {
 		_, err := cliConnection.CliCommandWithoutTerminalOutput("app", args[1])
-		if err != nil {
-			fmt.Fprintln(os.Stdout, "error: app does not exist in this org/space")
-			os.Exit(1)
-		}
+		fatalWithMessageIf(err, "app does not exist in this org/space")
 		appName := args[1]
 		guid := findAppGUID(cliConnection, appName)
 		fmt.Println("App GUID:", guid)
+
+		logstash, err := findLogstashInAppServices(cliConnection, guid)
+		fatalIf(err)
+
+		fmt.Printf("%#v\n", logstash)
 	}
 
 }
@@ -92,4 +116,24 @@ func findAppGUID(cliConnection plugin.CliConnection, appName string) string {
 	json.Unmarshal([]byte(strings.Join(output, "")), &res)
 
 	return res.Resources[0].Metadata.GUID
+}
+
+func findLogstashInAppServices(cliConnection plugin.CliConnection, appGUID string) (logstash appEnvService, err error) {
+	appQuery := fmt.Sprintf("/v2/apps/%v/env", appGUID)
+	cmd := []string{"curl", appQuery}
+	output, _ := cliConnection.CliCommandWithoutTerminalOutput(cmd...)
+	appEnvs := appEnv{}
+	json.Unmarshal([]byte(output[0]), &appEnvs)
+	str, err := json.Marshal(appEnvs.System["VCAP_SERVICES"])
+	if err != nil {
+		return
+	}
+	services := appEnvServices{}
+	json.Unmarshal([]byte(str), &services)
+	if len(services["logstash14"]) == 0 {
+		err = errors.New("app is not bound to a logstash14 service")
+		return
+	}
+	logstash = services["logstash14"][0]
+	return
 }
